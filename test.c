7a6aebebb4ce96640842b37b8f4c7ec79847703d
Zaynabi Jeyne lothston <zjeyne-l@e4r3p1.21-school.ru>
2019-03-02 19:12:14 +0300

    wolf

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: zjeyne-l <zjeyne-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/27 13:23:11 by zjeyne-l          #+#    #+#             */
-/*   Updated: 2019/03/02 15:48:10 by zjeyne-l         ###   ########.fr       */
+/*   Updated: 2019/03/02 19:11:04 by zjeyne-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@ void	ft_refresh_img(t_mlx *mlx)
 void	ft_draw_all(t_mlx *mlx)
 {
 	ft_draw_line(mlx, mlx->points[0].x, mlx->points[0].y, mlx->points[1].x, mlx->points[1].y, 0xFF00FF);
-	ft_draw_line(mlx, mlx->points[1].x, mlx->points[1].y, mlx->points[2].x, mlx->points[2].y, 0xFF00FF);
+	//ft_draw_line(mlx, mlx->points[1].x, mlx->points[1].y, mlx->points[2].x, mlx->points[2].y, 0xFF00FF);
 
-	ft_draw_line(mlx, mlx->player->x, mlx->player->y, mlx->player->x, mlx->player->y - 50, 0xFFFFFF);
+	ft_draw_line(mlx, mlx->player->x, mlx->player->y - 5, mlx->player->x, mlx->player->y - 50, 0xFFFFFF);
 	ft_draw_line(mlx, mlx->player->x, mlx->player->y, mlx->player->x, mlx->player->y, 0xFF00FF);
 }
 
@@ -32,33 +32,97 @@ void	ft_rotate(t_mlx *mlx, int dir)
 {
 	ft_refresh_img(mlx);
 
-	mlx->player->angle -= 0.1 * dir;
+	mlx->player->angle -= 0.01 * dir;
+	//printf("angle %f\n\n", mlx->player->angle);
 
 	int i = 0;
 	int j = 0;
-	while (i < 12)
-	{
-		double tx = mlx->points[j].x - mlx->player->x;
-		double ty = mlx->points[j].y - mlx->player->y;
+	//printf("x %f			y %f\n", mlx->points[0].x, mlx->points[0].y);
+	while (i < 4)
+	{		
+		double tx0 = mlx->points[0].x - mlx->player->x;
+		double ty0 = mlx->points[0].y - mlx->player->y;
 
-		double tz = tx * cos(0.05 * dir) + ty * sin(0.05 * dir);
+		double tx1 = mlx->points[1].x - mlx->player->x;
+		double ty1 = mlx->points[1].y - mlx->player->y;
 
-		tx = tx * sin(0.05 * dir) - ty * cos(0.05 * dir);
+		double tx2 = mlx->points[2].x - mlx->player->x;
+		double ty2 = mlx->points[2].y - mlx->player->y;
 
-		mlx->points[j].x = mlx->player->x - tx;
-		mlx->points[j].y = mlx->player->y - tz;
+		double tz0 = tx0 * cos(0.01 * dir) + ty0 * sin(0.01 * dir);
+		double tz1 = tx1 * cos(0.01 * dir) + ty1 * sin(0.01 * dir);
+		double tz2 = tx2 * cos(0.01 * dir) + ty2 * sin(0.01 * dir);
+
+		tx0 = tx0 * sin(0.01 * dir) - ty0 * cos(0.01 * dir);
+		tx1 = tx1 * sin(0.01 * dir) - ty1 * cos(0.01 * dir);
+		tx2 = tx2 * sin(0.01 * dir) - ty2 * cos(0.01 * dir);
+
+
+		mlx->points[0].x = mlx->player->x - tx0;
+		mlx->points[0].y = mlx->player->y - tz0;
+
+		mlx->points[1].x = mlx->player->x - tx1;
+		mlx->points[1].y = mlx->player->y - tz1;
+
+		mlx->points[2].x = mlx->player->x - tx2;
+		mlx->points[2].y = mlx->player->y - tz2;
+
+
+		if (i == 3)
+		{
+			double x0 = -tx0 * 16 / tz0;
+			double y0a = -mlx->player->x / tz0;
+			double y0b = mlx->player->y / tz0;
+
+			double x1 = -tx1 * 16 / tz1;
+			double y1a = -mlx->player->x / tz1;
+			double y1b = mlx->player->y / tz1;
+
+			double x2 = -tx2 * 16 / tz2;
+			double y2a = -mlx->player->x / tz2;
+			double y2b = mlx->player->x / tz2;
+
+			ft_draw_line(mlx, mlx->player->x + x0, mlx->player->y + y0a, mlx->player->x + x1, mlx->player->y + y1a, 0xFF00FF);
+			ft_draw_line(mlx, mlx->player->x + x0, mlx->player->y + y0b, mlx->player->x + x1, mlx->player->y + y1b, 0xFF00FF);
+			ft_draw_line(mlx, mlx->player->x + x0, mlx->player->y + y0a, mlx->player->x + x0, mlx->player->y + y0b, 0xFF00FF);
+			ft_draw_line(mlx, mlx->player->x + x1, mlx->player->y + y1a, mlx->player->x + x1, mlx->player->y + y1b, 0xFF00FF);
+
+			// ft_draw_line(mlx, mlx->player->x + x1, mlx->player->y + y1a, mlx->player->x + x2, mlx->player->y + y2a, 0xFF00FF);
+			// ft_draw_line(mlx, mlx->player->x + x1, mlx->player->y + y1b, mlx->player->x + x2, mlx->player->y + y2b, 0xFF00FF);
+			// ft_draw_line(mlx, mlx->player->x + x1, mlx->player->y + y1a, mlx->player->x + x1, mlx->player->y + y1b, 0xFF00FF);
+			// ft_draw_line(mlx, mlx->player->x + x2, mlx->player->y + y2a, mlx->player->x + x2, mlx->player->y + y2b, 0xFF00FF);
+		}
 
 		i++;
 		j++;
 		(j > 2) ? j = 0 : 1;
 	}
+	//printf("x %f			y %f\n", mlx->points[0].x, mlx->points[0].y);
 
 	ft_draw_all(mlx);
 
 	mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img, 0, 0);
 }
 
-void	ft_move(t_mlx *mlx, int dir)
+void	ft_move_x(t_mlx *mlx, int dir)
+{
+	ft_refresh_img(mlx);
+
+	int i = 0;
+	while (i < 3)
+	{
+		mlx->points[i].x += 25 * dir;
+		i++;
+	}
+
+	ft_rotate(mlx, 0);
+
+	ft_draw_all(mlx);
+
+	mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img, 0, 0);
+}
+
+void	ft_move_y(t_mlx *mlx, int dir)
 {
 	ft_refresh_img(mlx);
 
@@ -68,6 +132,8 @@ void	ft_move(t_mlx *mlx, int dir)
 		mlx->points[i].y += 25 * dir;
 		i++;
 	}
+
+	ft_rotate(mlx, 0);
 
 	ft_draw_all(mlx);
 
@@ -87,6 +153,8 @@ void	ft_start(t_mlx *mlx)
 	mlx->player->y = H / 2;
 	mlx->player->angle = 0;
 
+	ft_rotate(mlx, 1);
+
 	ft_draw_all(mlx);
 
 	mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img, 0, 0);
@@ -102,10 +170,12 @@ int		mouse_press(int button, int x, int y, void *param)
 int		key_press(int keycode, t_mlx *mlx)
 {
 	(keycode == 53) ? exit(0) : 1;
-	(keycode == 0) ? ft_rotate(mlx, -1) : 1;
-	(keycode == 2) ? ft_rotate(mlx, 1) : 1;
-	(keycode == 13) ? ft_move(mlx, 1) : 1;
-	(keycode == 1) ? ft_move(mlx, -1) : 1;
+	(keycode == 123) ? ft_rotate(mlx, -1) : 1;
+	(keycode == 124) ? ft_rotate(mlx, 1) : 1;
+	(keycode == 13) ? ft_move_y(mlx, 1) : 1;
+	(keycode == 1) ? ft_move_y(mlx, -1) : 1;
+	(keycode == 0) ? ft_move_x(mlx, 1) : 1;
+	(keycode == 2) ? ft_move_x(mlx, -1) : 1;
 	if (keycode == 15)
 	{
 		ft_refresh_img(mlx);
